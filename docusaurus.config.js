d70b2866251f4b5e83968c48a62568f878645218
Callista <callistachow30@gmail.com>
2025-03-21 11:38:44 +0700

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://edelweiss-documentation.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/edelweiss/',
+  baseUrl: '/edelweiss-documenation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
